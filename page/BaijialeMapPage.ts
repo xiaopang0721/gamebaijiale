a7c239e7b088200c81342715e3619354076ca907
xiaoer <xiaoer@example.com>
2019-10-18 12:00:46 +0800

    修改当祈福动画飘过去后，在显示祈福头像

--- page/BaijialeMapPage.ts
+++ page/BaijialeMapPage.ts
@@ -390,10 +390,11 @@ module gamebaijiale.page {
                             this._viewUI.main_player.img_qifu.visible = true;
                             this._viewUI.main_player.img_icon.skin = TongyongUtil.getHeadUrl(mainUnit.GetHeadImg(), 2);
                         })
-                    } else {
-                        this._viewUI.main_player.img_qifu.visible = true;
-                        this._viewUI.main_player.img_icon.skin = TongyongUtil.getHeadUrl(mainUnit.GetHeadImg(), 2);
-                    }
+                    } 
+                    // else {
+                    //     this._viewUI.main_player.img_qifu.visible = true;
+                    //     this._viewUI.main_player.img_icon.skin = TongyongUtil.getHeadUrl(mainUnit.GetHeadImg(), 2);
+                    // }
                 } else {
                     this._viewUI.main_player.img_icon.skin = TongyongUtil.getHeadUrl(mainUnit.GetHeadImg(), 2);
                     this._viewUI.main_player.img_qifu.visible = false;
@@ -1333,10 +1334,11 @@ module gamebaijiale.page {
                                 seat.img_qifu.visible = true;
                                 seat.img_icon.skin = TongyongUtil.getHeadUrl(unit.GetHeadImg(), 2);
                             })
-                        } else {
-                            seat.img_qifu.visible = true;
-                            seat.img_icon.skin = TongyongUtil.getHeadUrl(unit.GetHeadImg(), 2);
-                        }
+                        } 
+                        // else {
+                        //     seat.img_qifu.visible = true;
+                        //     seat.img_icon.skin = TongyongUtil.getHeadUrl(unit.GetHeadImg(), 2);
+                        // }
                     } else {
                         seat.img_qifu.visible = false;
                     }
